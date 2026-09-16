25c9c68ba82b56080c874f8f8f7330bfeca4d380
Nurbagus Saputro <114718404+Bagus-Saputra@users.noreply.github.com>
2026-09-16 13:08:26 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -170,11 +170,16 @@ export default function SurprisePage() {
                   First-time secara tiba-tiba banget ngajak pacaran dan itu membuatku kaget dan bingung sambil berpikir
                   "ini orang kenapa tiba-tiba ngajak pacaran dan padahal dia gak tau riwayatku dulu di game ini"
                   sambil tertawa dan aku mencoba buat memberanikan diri untuk mengatakan iya berpacaran, kemudian 
-                  belakangan ini aku masih kebingungan juga bagaimana cara menyikapi orang yang suka tiba-tiba moodswing,
-                  kemudian dan yahh, akhirnya aku membuatkan web ini untuk kamu. aku gak tau cara membuktikan rasa cinta ini
+                  belakangan ini aku masih kebingungan bagaimana cara menyikapi orang yang suka tiba-tiba kek pantek tapi kadang lucu juga tingkahnya.
+                  dan yahh, akhirnya aku membuatkan web ini buat kamu.
+                </p>
+                <p style={styles.paragraph}>
+                  aku gak tau cara membuktikan rasa cinta ini
                   tapi dengan membuat ini aku serius untuk lanjut ke hubungan pacaran, tapi maaf dengan sikapku yang kaku dan suka diem 
-                  ini aku sedang pengen liat sikap dan tingkah lucumu itu hehehe, maaf yaa cok 🐒
-                  . entah ini excited atau bukan, dan biasa aja atau bukan tapi pukimaklah capek aku bikin kata-kata serius anjeng
+                  ini aku sedang pengen liat sikap dan tingkah lucumu itu hehehe, maaf yaa cok 🐒.
+                </p>
+                <p style={styles.paragraph}>
+                  entah ini excited atau bukan, dan biasa aja atau bukan tapi pukimaklah capek aku bikin kata-kata serius ini anjeeeng
                 </p>
 
                 <p style={styles.paragraph}>
